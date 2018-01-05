c752d6014cda8d2295e4ddb277fc774936aabe76
carreerit <rwn0517@gmail.com>
2018-01-05 08:16:50 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,3 +4,5 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 yum install java jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
